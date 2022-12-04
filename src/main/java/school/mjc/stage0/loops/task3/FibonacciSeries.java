dcc63ede5ca0d9ec160f3c5aa3d8a7bef30a8d79
Sarvarbek <112868538+Mr-Sarvarbek@users.noreply.github.com>
2022-12-04 15:20:06 +0500

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int f0=0,f1=1,fn=0;
+        for (int i = 0; i <= lastFibonacci; i++) {
+            fn=f0+f1;
+            f0=f1;
+            f1=fn;
+        }
+        System.out.println(fn);
     }
 }
