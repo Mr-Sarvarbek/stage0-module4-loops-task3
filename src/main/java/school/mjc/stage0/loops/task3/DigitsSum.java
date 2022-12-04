c9d2fa02e4358cedc37eb5de419f2471dad772ac
Sarvarbek <112868538+Mr-Sarvarbek@users.noreply.github.com>
2022-12-04 15:19:34 +0500

    Update DigitsSum.java

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
+        int number = Math.abs(t);
+        int sum;
+        for(sum=0; number!=0; number=number/10)
+        {
 
+            sum = sum + number % 10;
+        }
+
+        System.out.println(sum);
     }
 }
